a633a2a6a22c3bd854ace5ca4baafe3e4773485a
Supachai Keemthong <supachai@smartcontractthailand.com>
2019-08-12 16:38:01 +0700

    initial project

---
+++ app.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"net/http"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+	"encoding/json"
+	"github.com/gorilla/mux"
+)
+
+type Developer struct {
+	ID string 
+	Name string
+}
+
+func getDevelopers() []Developer{
+	session, err := mgo.Dial("mongodb://127.0.0.1:27017/testDB")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("testDB").C("developers")
+	developers := []Developer{}
+	err = c.Find(nil).All(&developers)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return developers
+}
+
+func getDeveloper(id string) Developer{
+	session, err := mgo.Dial("mongodb://127.0.0.1:27017/testDB")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	c := session.DB("testDB").C("developers")
+	developer := Developer{}
+	err = c.Find(bson.M{"id": id}).One(&developer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return developer
+
+}
+
+func removeDeveloper(id string) Developer{
+	session, err := mgo.Dial("mongodb://127.0.0.1:27017/testDB")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	c := session.DB("testDB").C("developers")
+	developer := Developer{}
+	err = c.Remove(bson.M{"id": id})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return developer
+
+}
+
+func createDeveloper(e Developer) error{
+	session, err := mgo.Dial("mongodb://127.0.0.1:27017/testDB")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	c := session.DB("testDB").C("developers")
+	return c.Insert(e)
+}
+
+func updateDeveloper(e Developer) error {
+	session, err := mgo.Dial("mongodb://127.0.0.1:27017/testDB")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	c := session.DB("testDB").C("developers")
+	return c.Update(e.ID, "0000")
+}
+
+func getAllDevelopers(res http.ResponseWriter, req *http.Request) {
+	developers := getDevelopers()
+	json.NewEncoder(res).Encode(developers)
+}
+
+func getOneDeveloper(res http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	developer := getDeveloper(params["id"])
+	json.NewEncoder(res).Encode(developer)
+}
+
+func postDeveloper(res http.ResponseWriter, req *http.Request){
+	var item Developer
+	json.NewDecoder(req.Body).Decode(&item)
+	createDeveloper(item)
+}
+
+func putDeveloper(res http.ResponseWriter, req *http.Request) {
+	var item Developer
+	json.NewDecoder(req.Body).Decode(&item)
+	updateDeveloper(item)
+}
+
+
+func DeleteDeveloper(res http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	removeDeveloper(params["id"])
+	res.Write([]byte("OK"))
+}
+
+func handleRequest() {
+	router := mux.NewRouter()
+	router.HandleFunc("/getAllDevelopers", getAllDevelopers).Methods("GET")
+	router.HandleFunc("/getDeveloper/{id}", getOneDeveloper).Methods("GET")
+	router.HandleFunc("/postDeveloper", postDeveloper).Methods("POST")
+	router.HandleFunc("/putDeveloper", putDeveloper).Methods("PUT")
+	router.HandleFunc("/removeDeveloper/{id}", DeleteDeveloper).Methods("DELETE")
+	http.ListenAndServe(":8080", router)
+}
+
+func main()  {
+	handleRequest()
+}
---
+++ go.mod
@@ -0,0 +1,6 @@
+module mongoAPI
+
+require (
+	github.com/gorilla/mux v1.7.3
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
+)
